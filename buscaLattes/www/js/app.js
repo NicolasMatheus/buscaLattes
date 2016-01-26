a7701aba06fa8404456fd267f9a6b1a9d765a58e
NicolasMatheus <nicolasgredes@gmail.com>
2016-01-26 17:50:18 -0300

    Desenvolvimento Busca Lattes

--- buscaLattes/www/js/app.js
+++ buscaLattes/www/js/app.js
@@ -6,68 +6,83 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('lattes', ['ionic', 'lattes.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
+.run(function($ionicPlatform)
+{
+  $ionicPlatform.ready(function()
+  {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard)
+    {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
     }
-    if (window.StatusBar) {
+    if (window.StatusBar)
+    {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider)
+{
   $stateProvider
 
-    .state('app', {
+    .state('app',
+    {
     url: '/app',
     abstract: true,
     templateUrl: 'templates/menu.html',
     controller: 'LattesCtrl'
   })
 
-  .state('app.search', {
+  .state('app.search',
+  {
     url: '/search',
     views: {
-      'menuContent': {
+      'menuContent':
+      {
         templateUrl: 'templates/search.html'
       }
     }
   })
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
+  .state('app.home',
+  {
+      url: '/home',
+      views:
+      {
+        'menuContent':
+        {
+          templateUrl: 'templates/home.html'
         }
       }
     })
-    .state('app.playlists', {
-      url: '/playlists',
+    .state('app.professores',
+    {
+      url: '/professores',
       views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
+        'menuContent':
+        {
+          templateUrl: 'templates/professores.html',
           controller: 'PlaylistsCtrl'
         }
       }
     })
 
-  .state('app.single', {
+  .state('app.single',
+  {
     url: '/playlists/:playlistId',
     views: {
-      'menuContent': {
+      'menuContent':
+      {
         templateUrl: 'templates/playlist.html',
         controller: 'PlaylistCtrl'
       }
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/home');
 });
