2ed85b8cf46df352d2786abeeeccfac50f80c692
NicolasMatheus <nicolasgredes@gmail.com>
2016-06-21 16:33:24 -0300

    Adicionado um novo controller para gerenciar os dados dos professores

--- buscaLattes/www/js/controllers/controllers.js
+++ buscaLattes/www/js/controllers/controllers.js
@@ -1,71 +1,51 @@
 angular.module('lattes.controllers', [])
 
-.controller('ProfessoresCtrl', function($scope, $http, $stateParams)
-{
-  var carregarProfessores = function ()
-  {
-          var URL = "http://200.129.176.42:8085/Lattes1/json/buscaLattes/professores.json";
-          var url = "http://10.101.0.251:8085/Lattes1/json/buscaLattes/professores.json";
+.controller('ProfessoresCtrl', function($scope, $http, $stateParams) {
+  var carregarProfessores = function () {
+    const URL = "http://200.129.176.42:8085/Lattes1/json/buscaLattes/professores.json";
+    const url = "http://10.101.0.251:8085/Lattes1/json/buscaLattes/professores.json";
 
-              console.log(URL);
-          $http.get(URL)
-            .success(function (data)
-            {
-                  $scope.prof = data;
-            })
-            .error(function(erro)
-            {
-              $http.get(url)
-                .success(function (data)
-                {
-                  $scope.prof = data;
-                });
-            });
+    $http.get(URL).success(function (data) {
+      $scope.prof = data;
+    });
+    $http.get(url).success(function (data) {
+      $scope.prof = data;
+    });
+
   };
-
-  $scope.$on('$ionicView.enter', function()
-  {
+  $scope.$on('$ionicView.enter', function() {
     carregarProfessores();
   });
 })
-
-.controller('ProfessoreCtrl', function($scope, $http, $stateParams)
-{
-var pegarJson = function (){
-
-    $scope.choice = true;
-
-            $scope.nomeProfessor = $stateParams.nome;
-            var idProfessor = $stateParams.id;
-
-
-            var requestNome = "http://10.101.0.251:8085/Lattes1/json/buscaLattes/"+ idProfessor +"/"+ idProfessor +".json";
-            var requestName = "http://200.129.176.42:8085/Lattes1/json/buscaLattes/"+ idProfessor +"/"+ idProfessor + ".json";
-
-            var requestImagem = "http://10.101.0.251:8085/Lattes1/json/buscaLattes/"+ idProfessor +"/"+ idProfessor +".gif";
-          var requestPicture = "http://200.129.176.42:8085/Lattes1/json/buscaLattes/"+ idProfessor +"/"+ idProfessor +".gif";
-
-            $http.get(requestName,requestPicture)
-                    .success(function (data)
-                    {
-                        $scope.selected = data;
-                        $scope.picProfessor = requestPicture;
-                    })
-                    .error(function(erro)
-                    {
-                      $http.get(requestNome,requestImagem)
-                        .success(function (data)
-                        {
-                            $scope.selected = data;
-                            $scope.picProfessor = requestImagem;
-                        });
-                    });
-                    console.log(requestNome);
-                    console.log(requestName);
-          };
-
-  $scope.$on('$ionicView.enter', function()
-  {
+.controller('ProfessoreCtrl', function($scope, $http, $stateParams, DadosCurriculo) {
+  var pegarJson = function (){
+    $scope.nomeProfessor = $stateParams.nome;
+    var idProfessor = $stateParams.id;
+    var requestNome = "http://10.101.0.251:8085/Lattes1/json/buscaLattes/"+ idProfessor +"/"+ idProfessor +".json";
+    var requestName = "http://200.129.176.42:8085/Lattes1/json/buscaLattes/"+ idProfessor +"/"+ idProfessor + ".json";
+    var requestImagem = "http://10.101.0.251:8085/Lattes1/json/buscaLattes/"+ idProfessor +"/"+ idProfessor +".gif";
+    var requestPicture = "http://200.129.176.42:8085/Lattes1/json/buscaLattes/"+ idProfessor +"/"+ idProfessor +".gif";
+		DadosCurriculo.requestNome = requestNome;
+		DadosCurriculo.requestName = requestName;
+    $http.get(requestPicture).success(function (data) {
+      $scope.picProfessor = requestPicture;
+    });
+    $http.get(requestImagem).success(function(data) {
+      $scope.picProfessor = requestPicture;
+    });
+  };
+  $scope.$on('$ionicView.enter', function(){
     pegarJson();
+  });
+})
+.controller('DadosCtrl', function($scope, $http, $stateParams, DadosCurriculo) {
+  $scope.dg = {id_1 : false, id_2 : false, id_3 : false, id_4 : false};
+	var requestNome = DadosCurriculo.requestNome;
+	var requestName = DadosCurriculo.requestName;
+	$http.get(requestName).success(function(data) {
+		$scope.dadosCurriculo = data;
+  });
+  $http.get(requestNome).success(function(data) {
+    $scope.dadosCurriculo = data;
   });
 });
