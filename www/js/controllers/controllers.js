054f5b6f85e1d133d9748b1e4fdbbb92aa73bf18
NicolasMatheus <nicolasgredes@gmail.com>
2016-08-26 01:44:01 -0300

    Adicionado mais id.id_

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -38,17 +38,19 @@ angular.module('lattes.controllers', [])
 })
 .controller('DadosCtrl', function($scope, $http, $stateParams, DadosCurriculo) {
   $scope.id = {
-    id_1 : false,
-    id_2 : false,
-    id_3 : false,
-    id_4 : false,
-    id_5 : false,
-    id_6 : false,
-    id_7 : false,
-    id_8 : false,
-    id_9 : false,
-    id_10 : false
+    id_1 : false, id_2 : false, id_3 : false, id_4 : false,
+    id_5 : false, id_6 : false, id_7 : false, id_8 : false,
+    id_9 : false, id_10 : false, id_11 : false, id_12 : false,
+    id_13 : false, id_14 : false, id_15 : false, id_16 : false,
+    id_17 : false, id_18 : false, id_19 : false, id_20 : false,
+    id_21 : false, id_22 : false, id_23 : false, id_24 : false,
+    id_25 : false, id_26 : false, id_27 : false, id_28 : false,
+    id_29 : false, id_30 : false, id_31 : false, id_32 : false,
+    id_33 : false, id_34 : false, id_35 : false, id_36 : false,
+    id_37 : false, id_38 : false, id_39 : false, id_40 : false
+    //id_17 : false, id_18 : false, id_19 : false, id_20 : false
   };
+
 	var requestNome = DadosCurriculo.requestNome;
 	var requestName = DadosCurriculo.requestName;
 	$http.get(requestName).success(function(data) {
